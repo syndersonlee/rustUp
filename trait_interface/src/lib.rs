29be40f2d5086c0953d0f96a497f46f7dce651f8
syndersonLEE <winnery93@gmail.com>
2022-12-15 23:36:46 +0900

    feat : rust trait

---
+++ trait_interface/src/lib.rs
@@ -0,0 +1,56 @@
+// extern crate aggregator;
+
+// use aggregator::Summarizable;
+
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+
+
+struct WeatherForecast {
+    high_temp : f64,
+    low_temp : f64,
+    chance_of_precipitation : f64,
+}
+
+impl Summarizable for WeatherForecast {
+    fn summary(&self) -> String {
+        format!("The high will be {}, and the low will be {}. The chance of precipitation is {}%", self.high_temp, self.low_temp, self.chance_of_precipitation)
+    }
+}
+
+pub struct NewsArticle {
+    pub headline : String,
+    pub location : String,
+    pub author : String,
+    pub content : String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username : String,
+    pub content : String,
+    pub reply : bool,
+    pub retweet : bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    } 
+}
+
+// let tweet = Tweet {
+//     username : String::from("horse_ebooks"),
+//     content : String::from("of course, as you probably already know, people"),
+//     reply : false,
+//     retweet : false,
+// };
+
+// println!("1 new tweet: {}", tweet.summary());
---
+++ trait_interface/src/main.rs
@@ -0,0 +1,25 @@
+use std::cmp::PartialOrd;
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let numbers = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&numbers);
+    println!("The largest number is {}", result);
+
+    let chars = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&chars);
+    println!("The largest char is {}", result);
+}

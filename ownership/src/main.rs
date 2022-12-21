eeb51309bbcb3b1a9d8dd433292c25296d4b0a8d
syndersonLEE <winnery93@gmail.com>
2022-12-21 23:37:55 +0900

    feat : dingle reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let s = String::from("hello");
     takes_ownership(s);
     let x = 5;                      
-    makes_copy(x);     
+    makes_copy(x);
 }
 
 fn takes_ownership (some_string : String) {

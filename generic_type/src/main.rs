a26ab7fa3412c8a29878da94a872ce39acb47ac6
syndersonLEE <winnery93@gmail.com>
2023-01-24 22:52:31 +0900

    feat : rust ch8

--- generic_type/src/main.rs
+++ generic_type/src/main.rs
@@ -58,6 +58,7 @@ fn main() {
     let p1 = Point { x: 5, y: 10.4 };
     let p2 = Point { x: "Hello", y: 'c'};
 
+    
     let p3 = p1.mixup(p2);
 
     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
